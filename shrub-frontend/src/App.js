0a3b3742a9c7e1162b7089eda3691c6319905955
Daniel Bolis <daniel@Daniels-MacBook-Pro-2.local>
2021-08-28 19:03:34 -0400

    implemented fetch from json server

--- shrub-frontend/src/App.js
+++ shrub-frontend/src/App.js
@@ -1,31 +1,36 @@
-import Navbar from "./components/Navbar";
-import About from "./components/About";
 import BuyCover from "./components/BuyCover";
-import Home from "./components/Home";
-import Steak from "./components/Steak";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
 
 
 function App() {
+  
+  
   return (
-    <Router>
+    // <Router>
       
         
-        <Navbar />
-      <div className="container-fluid">
-        <Switch>
-          <Route path="/about" component={About}/>
-          <Route path="/buycover" component={BuyCover}/>
-          <Route path="/" exact component={Home}/>
-          <Route path="/steak" component={Steak}/>
-        </Switch>
+    //     <Navbar />
+    //   <div className="container-fluid">
+    //     <Switch>
+    //       <Route path="/about" component={About}/>
+    //       <Route path="/buycover" component={BuyCover}/>
+    //       <Route path="/" exact component={Home}/>
+    //       <Route path="/steak" component={Steak}/>
+    //     </Switch>
 
-        {/* <div className="container-fluid">
-          <h1>Hello</h1>  
-        </div> */}
+    //     {/* <div className="container-fluid">
+    //       <h1>Hello</h1>  
+    //     </div> */}
 
-      </div>
-    </Router>
+    //   </div>
+    // </Router>
+
+    <>
+    
+      <BuyCover />
+    </>
   );
 }
 
--- shrub-frontend/src/components/BuyCover.js
+++ shrub-frontend/src/components/BuyCover.js
@@ -1,8 +1,70 @@
+import useStyles from "./styles";
+import {useState, useEffect} from "react";
+
+
+import {Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core'
+
+
 const BuyCover = () => {
+
+    const [policies, setPolicies] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/policies')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setPolicies(data)
+            });
+    }, []);
+
+    const classes = useStyles();
     return (
-        <div>
-            <h1>BuyCover</h1>
-        </div>
+        <>
+        <CssBaseline />
+        <main>
+          <div className={classes.container}>
+            <Container maxWidth="sm">
+              <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                Shrub
+              </Typography>
+              <Typography variant="h5" align="center" color="textSecondary" gutterBottom>
+                Please Pick a Policy
+              </Typography>
+            </Container>
+          </div>
+          <Container className={classes.cardGrid} maxWidth="md">
+            <Grid container spacing={4}>
+                {policies && policies.map((policy) => (
+                    <Grid item key={policy.id} xs={12} sm={6} md={4}>
+                        <Card className={classes.card}>
+                        <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="image title"/>
+                        <CardContent className={classes.cardContent}>
+                            <Typography gutterBottom variant="h5" align="center">
+
+                            {policy.location}
+                            </Typography>
+                            <Typography>
+                            Magnitude: {policy.magnitude} <br />
+                            Policy begins: {policy.begin} <br />
+                            Policy ends: {policy.end} <br />
+                            </Typography>
+                        </CardContent>
+                        <CardActions style={{justifyContent: 'center'}}>
+                            
+                            <Button size="small" color="primary">Purchase</Button>
+                            
+                            
+                        </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+          </Container>
+  
+        </main>
+      </>
     );
 }
 
