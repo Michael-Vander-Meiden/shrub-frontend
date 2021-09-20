050c59a8d7ebe62a8e0ab594e32c5d82d59ad95a
Daniel Bolis <daniel@Daniels-MacBook-Pro-2.local>
2021-09-20 17:37:05 -0400

    added search function

--- shrub-frontend/src/components/PolicyList.js
+++ shrub-frontend/src/components/PolicyList.js
@@ -3,11 +3,13 @@ import {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 
 
-import {Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core'
+import {Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, TextField} from '@material-ui/core'
 
 const PolicyList = () => {
 
     const [policies, setPolicies] = useState(null);
+    const [SearchTerm, setSearchTerm] = useState('');
+    const classes = useStyles();
 
     useEffect(() => {
         fetch('http://localhost:3000/policies')
@@ -19,13 +21,17 @@ const PolicyList = () => {
             });
     }, []);
 
-    const classes = useStyles();
+    
     return (
         <>
+
+
         <CssBaseline />
         <main>
-          <div className={classes.container}>
-            <Container maxWidth="sm">
+
+
+          <div>
+            <Container maxWidth="sm" className={classes.container}>
               <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                 Shrub
               </Typography>
@@ -34,9 +40,27 @@ const PolicyList = () => {
               </Typography>
             </Container>
           </div>
+          <div>
+          <Container className={classes.searchBar} align="center">
+            <TextField
+              label="Search a Location" 
+              variant="outlined"
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+              fullWidth
+            />
+          </Container>
+          </div>
           <Container className={classes.cardGrid} maxWidth="md">
             <Grid container spacing={4}>
-                {policies && policies.map((policy) => (
+                {policies && policies.filter((val) => {
+                    if (SearchTerm == "") {
+                      return val
+                    } else if (val.location.toLowerCase().startsWith(SearchTerm.toLocaleLowerCase())) {
+                      return val
+                    }
+                  }).map((policy) => (
                     <Grid item key={policy.id} xs={12} sm={6} md={4}>
                         <Card className={classes.card}>
                         <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="image title"/>
